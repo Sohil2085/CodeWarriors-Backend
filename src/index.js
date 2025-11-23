f5bd380b59307cf545baab8b3e92a2925432a791
Sohil2085 <sohilkareliya9695@gmail.com>
2025-11-24 00:46:40 +0530

    changing in cors policy

--- src/index.js
+++ src/index.js
@@ -12,10 +12,13 @@ import problemRoutes from "./routes/problem.routes.js";
 dotenv.config()
 
 const app = express();
-app.use(cors({ 
-  origin: ["http://localhost:5173"], 
-  credentials: true 
-}));
+app.use(
+  cors({
+    origin: ["https://code-warriors-lyart.vercel.app","http://localhost:5173"],   // your frontend domain
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
